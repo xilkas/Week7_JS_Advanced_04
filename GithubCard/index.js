38d3d02f44d1db7fe7cf781a5a21fb393dba3066
Fathi <fathi@Fathis-MacBook-Air.local>
2022-12-03 10:35:19 -0500

    finished

--- GithubCard/index.js
+++ GithubCard/index.js
@@ -3,11 +3,7 @@
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
-*/
 
-
-
-/*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
@@ -35,6 +31,90 @@
     </div>
 */
 
+
+
+
+      function createGitCard(data){
+
+         const MainDiv = document.createElement("div")
+         const userImage = document.createElement("img")
+         const cardDiv = document.createElement("div")
+         const userTitle = document.createElement("h3")
+         const userName = document.createElement("p")
+         const userLocation = document.createElement("p")
+         const userProfile = document.createElement("p")
+         const userAddress = document.createElement("a")
+         const userFollowers = document.createElement("p")
+         const userFollowing = document.createElement("p")
+         const userBio = document.createElement("p")
+      
+
+
+
+   // add data 
+
+ userImage.src= data.avatar_url;
+ userTitle.textContent = data.login;
+ userBio.textContent = "Bio : "   +   data.bio;
+ userAddress.textContent = data.url;
+ userFollowers.textContent = " followers : " + data.followers;
+ userFollowing.textContent= "Following: "   +  data.following;
+
+
+
+ // add clasess
+
+ MainDiv.classList.add("card")
+ cardDiv.classList.add("card-info")
+ userTitle.classList.add("name")
+ userName.classList.add("userName")
+
+   // connecting all elements using (appenChild)
+
+
+
+   userProfile.appendChild(userAddress)
+
+
+ cardDiv.appendChild(userTitle)
+ cardDiv.appendChild(userName)
+ cardDiv.appendChild(userLocation)
+ cardDiv.appendChild(userProfile)
+ cardDiv.appendChild(userFollowers)
+ cardDiv.appendChild(userFollowing)
+ cardDiv.appendChild(userBio)
+
+ MainDiv.appendChild(userImage)
+ MainDiv.appendChild(cardDiv)
+
+
+ return MainDiv
+
+
+
+ }
+
+
+
+  // call Api using Axios and connect the fucntion to the root element in the index.html
+
+
+
+ const entaryCards = document.querySelector(".cards")
+
+
+
+
+
+
+ axios.get(" https://api.github.com/users/xilkas")
+ .then(res => {
+   entaryCards.appendChild(createGitCard(res.data))
+
+ })
+ .catch(erro => {
+   console.log(erro)
+ });
 
 
 /*
